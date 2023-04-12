7e43cf8b70a174f8cf6cc70474e67bd8cd79ac9c
Abdullah al-zatar <abdulah.al-zatar@pwc.com>
2023-04-12 14:13:28 +0300

    add fomik

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -4,6 +4,7 @@ import mark from "../Assets/mark.svg";
 import CardRecipe from "./CardRecipe.js";
 import { useState,useEffect } from "react";
 import axios from 'axios';
+ import { Formik } from 'formik';
 
 function MainContent() {
   
@@ -168,40 +169,74 @@ useEffect(() => {
         </div>
 
         <div className={toggleState === 3 ? "content  active-content" : "content"}>
+        
         </div>
 
         <div className={toggleState === 4 ? "content  active-content" : "content"}>
         <div className="contusform">
-  <p className="bgcount">Contact us</p>
-  <p className="smcount">Send us a message, complaint or inquiry by filling the contact form below.</p>
-            
-          <form className="counfrom" onSubmit={handleSubmit}>
-            <div>
-            <div className="parent">
-<div className="maincoun"></div>
-<div className="div2"><div className={toggle2State === 2 ? "errormain" :"tdinputmain" }>First Name<div><input type="text" className="firstname" placeholder='EnterFirstName'></input ></div></div><div className="tdtext2"><p className={toggle3State === 2 ? "smallfr": "smallfrn"  }>This fieldis required</p></div> </div>
-<div className="div3"><div className={toggle2State === 3 ? "errormain" :"tdinputmain" }>Last Name<div><input type="text" className="lastname" placeholder='EnterLastName'></input ></div></div><div className="tdtext2"><p className={toggle3State === 3 ? "smallfr": "smallfrn"  }> This fieldis required</p></div> </div>
-<div className="div4"><div className={toggle2State === 4 ? "errormain" :"tdinputmain" }>Email Address<div><input type="text" className="email" placeholder='Enter Email Address'></input ></div></div><div className="tdtext2"><p className={toggle3State === 4 ? "smallfr": "smallfrn"  }> This fieldis required</p></div>  </div>
-<div className="div5"><div className={toggle2State === 5 ? "errormain" :"tdinputmain" }>Phone Number<div><input type="tel" className="phone" placeholder="50XXXXXXXX"></input ></div></div><div className="tdtext2"><p className={toggle3State === 5 ? "smallfr": "smallfrn"  }> This fieldis required</p></div></div>
-<div className="div6"><div className={toggle2State === 6 ? "errormain" :"tdinputmain" }>Message Type<div><input type="text" className="massgty" placeholder='Enter Message'></input ></div></div><div className="tdtext2"><p className={toggle3State === 6 ? "smallfr": "smallfrn"  }> This fieldis required</p></div>  </div>
-<div className="div7"><div className={toggle2State === 7 ? "errormain" :"tdinputmain" }>Message <div><textarea placeholder='Enter Message' rows="4" className="mass"></textarea></div></div><div className="tdtext2"><p className={toggle3State === 7? "smallfr": "smallfrn"  }> This fieldis required</p></div>  </div>
+        <p className="bgcount">Contact us</p>
+        <p className="smcount">Send us a message, complaint or inquiry by filling the contact form below.</p>
+        <Formik
+       initialValues={{ firstname: '', lastname: '', email: '', phone: '', massgty: '', mass: ''}}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         if(!values.firstname){
+          errors.firstname = 'Required';
+         }
+         if(!values.lastname){
+          errors.lastname = 'Required';
+         }
+         if(!values.phone){
+          errors.phone = 'Required';
+         }
+         if(!values.massgty){
+          errors.massgty = 'Required';
+         }
+         if(!values.mass){
+          errors.mass = 'Required';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form onSubmit={handleSubmit}>
+          <div className="parent">
 
-<div className="div8"><div className="maindivbut">
-    <div>
-<button className="butsum" type="submit">Send Message</button>
+<div className="div2"><div className="tdinputmain">First Name<div><input type="text" name="firstname" onChange={handleChange} onBlur={handleBlur} value={values.firstname} className="firstname" placeholder='EnterFirstName'></input ></div></div><div className="tdtext2"><p className= "smallfr">{errors.firstname && touched.firstname && errors.firstname}</p></div> </div>
+<div className="div3"><div className="tdinputmain">Last Name<div><input type="text"  name="lastname" onChange={handleChange} onBlur={handleBlur} value={values.lastname} className="lastname" placeholder='EnterLastName'></input ></div></div><div className="tdtext2"><p className= "smallfr">{errors.lastname && touched.lastname && errors.lastname}</p></div> </div>
+<div className="div4"><div className="tdinputmain">Email Address<div><input type="text"  name="email" onChange={handleChange} onBlur={handleBlur} value={values.email} className="email" placeholder='Enter Email Address'></input ></div></div><div className="tdtext2"><p className= "smallfr">{errors.email && touched.email && errors.email}</p></div>  </div>
+<div className="div5"><div className="tdinputmain">Phone Number<div><input type="tel"  name="phone" onChange={handleChange} onBlur={handleBlur} value={values.phone} className="phone" placeholder="50XXXXXXXX"></input ></div></div><div className="tdtext2"><p className= "smallfr">{errors.phone && touched.phone && errors.phone}</p></div></div>
+<div className="div6"><div className="tdinputmain">Message Type<div><input type="text" name="massgty" onChange={handleChange} onBlur={handleBlur} value={values.massgty} className="massgty" placeholder='Enter Message'></input ></div></div><div className="tdtext2"><p className= "smallfr">{errors.massgty && touched.massgty && errors.massgty}</p></div>  </div>
+<div className="div7"><div className="tdinputmain">Message <div><textarea name="mass" onChange={handleChange} onBlur={handleBlur} value={values.mass} placeholder='Enter Message' rows="4" className="mass"></textarea></div></div><div className="tdtext2"><p className= "smallfr">{errors.mass && touched.mass && errors.mass}</p></div>  </div>
+<div className="div8"><button className="butsum" type="submit" disabled={isSubmitting}>Send Message</button></div>
 </div>
-<div className={toggle4State === 1? "tdinputmass1n": "tdinputmass1"}><div className="imgtdi" ><img src={toggle5State === 1? mark: markx} alt='#'></img></div><div> <i className={toggle5State === 1? "errortext": "susses"}>Your message has been sent successfully. We will get back to you soon.</i> <i className={toggle5State === 1? "susses":"errortext" } style={{color: "#9D0D2D"}}>An error occurred while sending your message. Please try again.</i></div></div>
-</div></div>
 
-
-</div>
-            </div>
-
-
-          </form>
-
-
-          </div>
+         </form>
+       )}
+     </Formik>
+        </div>
 
         </div>
 
