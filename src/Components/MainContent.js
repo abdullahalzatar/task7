59244cbe531c38ea9be6891480eba5dad166354b
Abdullah al-zatar <abdulah.al-zatar@pwc.com>
2023-04-13 13:40:38 +0300

    add table

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -1,38 +1,107 @@
 import "../Style/MainContent.css"
-import markx from "../Assets/Default.svg";
-import mark from "../Assets/mark.svg";
 import CardRecipe from "./CardRecipe.js";
-import { useState,useEffect } from "react";
 import axios from 'axios';
+import * as React from "react";
+import { useTable } from "react-table";
  import { Formik } from 'formik';
+ import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function MainContent() {
-  
- const username = document.getElementsByClassName("firstname");
-const lastname = document.getElementsByClassName("lastname");
-const email = document.getElementsByClassName("email");
-const phone = document.getElementsByClassName("phone");
-const massgty = document.getElementsByClassName("massgty");
-const mass = document.getElementsByClassName("mass");
 
-const [values, setValue] = useState()
   
-useEffect(() => {
+  const fakeData =[
+    { 
+    id: "1",
+    name: "Apple",
+    calories: "52 Calories",
+    size: "100g",
+    },
+    { 
+      id: "2",
+      name: "Banana",
+      calories: "89 Calories",
+      size: "100g",
+      },
+      { 
+        id: "3",
+        name: "Avocado",
+        calories: "160 Calories ",
+        size: "100g",
+        },
+        { 
+          id: "4",
+          name: "Coconut",
+          calories: "354 Calories",
+          size: "100g",
+          },
+          { 
+            id: "5",
+            name: "Almonds",
+            calories: "576 Calories",
+            size: "100g",
+            },{ 
+              id: "6",
+              name: "Kaju",
+              calories: "553 Calories",
+              size: "100g",
+              },
+              { 
+                id: "7",
+                name: "Greek Yogurt",
+                calories: "59 Calories",
+                size: "100g",
+                }
+  
+  ]
 
+  const data = React.useMemo(() => fakeData, []);
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "id",
+      },
+      {
+        Header: "Food Name",
+        accessor: "name",
+      },
+      {
+        Header: "Calories",
+        accessor: "calories",
+      },
+      {
+        Header: "Size",
+        accessor: "size",
+      },
+      
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
+const [values, setValue] = React.useState()
+  
+React.useEffect(() => {
  axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=7aaf410a3da0448981ba295e58cd292d&number=10`)
 .then((response)=> setValue(response.data.results))
 .catch((err) => {console.log(err.message);});
 
 }, []);
   
-    const [toggleState, setToggleState] = useState(1);
+const [toggleState, setToggleState] = React.useState(1);
   
 
   const toggleTab = (index) => {
     setToggleState(index);
   };
-
- 
 
     return (      
         <div className="contactus"> 
@@ -60,11 +129,41 @@ useEffect(() => {
         </div>
 
         <div className={toggleState === 2 ? "content  active-content" : "content"}>
+        <div className="contusform">
+        <p className="bgcount">Diet Schedule</p>
+        
+        <Table sx={{ minWidth: 650  , bgcolor: "#363636"  , mt: 3, border: 1 ,borderColor: '#fff' ,borderRadius: 3  }} aria-label="simple table" {...getTableProps()}>
+          <TableHead>
+            {headerGroups.map((headerGroup) => (
+              <TableRow {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <TableCell {...column.getHeaderProps()} sx={{ color: "#fff" ,fontWeight: 'bold' }} >
+                    {column.render("Header")}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableHead>
+          <TableBody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <TableRow {...row.getRowProps()}  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  {row.cells.map((cell) => (
+                    <TableCell sx={{ color: "#fff"}} component="th" scope="row"  {...cell.getCellProps()}> {cell.render("Cell")} </TableCell >
+                  ))}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+       
+        </div>
+
         </div>
 
         <div className={toggleState === 3 ? "content  active-content" : "content"}>
-        
-        </div>
+         </div>
 
         <div className={toggleState === 4 ? "content  active-content" : "content"}>
         <div className="contusform">
@@ -76,11 +175,9 @@ useEffect(() => {
          const errors = {};
          if (!values.email) {
            errors.email = 'Required';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
+         } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)){
            errors.email = 'Invalid email address';
-         }
+          }
          if(!values.firstname){
           errors.firstname = 'Required';
          }
@@ -118,27 +215,85 @@ useEffect(() => {
          <form onSubmit={handleSubmit}>
           <div className="parent">
 
-<div className="div2"><div className="tdinputmain">First Name<div><input type="text" name="firstname" onChange={handleChange} onBlur={handleBlur} value={values.firstname} className="firstname" placeholder='EnterFirstName'></input ></div></div><div className="tdtext2"><p className= "smallfr">{errors.firstname && touched.firstname && errors.firstname}</p></div> </div>
-<div className="div3"><div className="tdinputmain">Last Name<div><input type="text"  name="lastname" onChange={handleChange} onBlur={handleBlur} value={values.lastname} className="lastname" placeholder='EnterLastName'></input ></div></div><div className="tdtext2"><p className= "smallfr">{errors.lastname && touched.lastname && errors.lastname}</p></div> </div>
-<div className="div4"><div className="tdinputmain">Email Address<div><input type="text"  name="email" onChange={handleChange} onBlur={handleBlur} value={values.email} className="email" placeholder='Enter Email Address'></input ></div></div><div className="tdtext2"><p className= "smallfr">{errors.email && touched.email && errors.email}</p></div>  </div>
-<div className="div5"><div className="tdinputmain">Phone Number<div><input type="tel"  name="phone" onChange={handleChange} onBlur={handleBlur} value={values.phone} className="phone" placeholder="50XXXXXXXX"></input ></div></div><div className="tdtext2"><p className= "smallfr">{errors.phone && touched.phone && errors.phone}</p></div></div>
-<div className="div6"><div className="tdinputmain">Message Type<div><input type="text" name="massgty" onChange={handleChange} onBlur={handleBlur} value={values.massgty} className="massgty" placeholder='Enter Message'></input ></div></div><div className="tdtext2"><p className= "smallfr">{errors.massgty && touched.massgty && errors.massgty}</p></div>  </div>
-<div className="div7"><div className="tdinputmain">Message <div><textarea name="mass" onChange={handleChange} onBlur={handleBlur} value={values.mass} placeholder='Enter Message' rows="4" className="mass"></textarea></div></div><div className="tdtext2"><p className= "smallfr">{errors.mass && touched.mass && errors.mass}</p></div>  </div>
-<div className="div8"><button className="butsum" type="submit" disabled={isSubmitting}>Send Message</button></div>
+<div className="div2">
+  <div className="tdinputmain">First Name
+    <div>
+      <input type="text" name="firstname" onChange={handleChange} onBlur={handleBlur} value={values.firstname} placeholder='EnterFirstName'/>
+    </div>
+  </div>
+  <div className="tdtext2">
+      <p className= "smallfr">{errors.firstname && touched.firstname && errors.firstname}</p>
+  </div>
 </div>
 
-         </form>
-       )}
-     </Formik>
+<div className="div3">
+  <div className="tdinputmain">Last Name
+    <div>
+      <input type="text"  name="lastname" onChange={handleChange} onBlur={handleBlur} value={values.lastname} placeholder='EnterLastName'/>
+    </div>
+  </div>
+  <div className="tdtext2">
+    <p className= "smallfr">{errors.lastname && touched.lastname && errors.lastname}</p>
+  </div> 
+</div>
+
+<div className="div4">
+  <div className="tdinputmain">Email Address
+    <div>
+      <input type="text"  name="email" onChange={handleChange} onBlur={handleBlur} value={values.email} placeholder='Enter Email Address'/>
+    </div>
+  </div>
+  <div className="tdtext2">
+    <p className= "smallfr">{errors.email && touched.email && errors.email}</p>
+  </div>
+</div>
+
+<div className="div5">
+  <div className="tdinputmain">Phone Number
+    <div>
+      <input type="tel"  name="phone" onChange={handleChange} onBlur={handleBlur} value={values.phone} className="phone" placeholder="50XXXXXXXX"/>
+    </div>
+  </div>
+  <div className="tdtext2">
+    <p className= "smallfr">{errors.phone && touched.phone && errors.phone}</p>
+  </div>
+</div>
+
+<div className="div6">
+  <div className="tdinputmain">Message Type
+    <div>
+      <input type="text" name="massgty" onChange={handleChange} onBlur={handleBlur} value={values.massgty} className="massgty" placeholder='Enter Message'/>
+    </div>
+  </div>
+  <div className="tdtext2">
+    <p className= "smallfr">{errors.massgty && touched.massgty && errors.massgty}</p>
+  </div> 
+</div>
+
+<div className="div7">
+  <div className="tdinputmain">Message 
+    <div>
+      <textarea name="mass" onChange={handleChange} onBlur={handleBlur} value={values.mass} placeholder='Enter Message' rows="4" className="mass"/>
+    </div>
+  </div>
+  <div className="tdtext2">
+    <p className= "smallfr">{errors.mass && touched.mass && errors.mass}</p>
+  </div>
+</div>
+
+<div className="div8">
+  <button className="butsum" type="submit" disabled={isSubmitting}>Send Message</button>
+</div>
+
+</div>
+</form>)}
+</Formik>
         </div>
 
         </div>
 
         </div> 
         </div>
-  
-
- 
     );
   }
   
